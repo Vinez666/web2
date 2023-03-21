f761496d8d587f0509e9c719fa66fc326dceec4a
Vinez666 <127708664+Vinez666@users.noreply.github.com>
2023-03-21 12:13:33 +0700

    commit

---
+++ phpdasar1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP Dasar</title>
+</head>
+<body>
+<h1>Belajar PHP Dasar</h1>
+<?php
+echo "Hello World";
+?>
+<br>
+<h2>Menggunakan Variable</h2>
+<?php
+$nim = "312110081";
+$nama = 'Kevin Suyadi Ritonga';
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama";
+?>
+</body>
+</html>
